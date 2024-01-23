0c02727d97916b1f70a475ccae2e8481718ffff5
welcomeglory <153584777+welcomeglory@users.noreply.github.com>
2024-01-23 16:01:37 +0900

    [level 0] Title: 배열 만들기 2, Time: 38.75 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/README.md
+++ 프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
 
 ### 성능 요약
 
-메모리: 89.7 MB, 시간: 27.18 ms
+메모리: 87.7 MB, 시간: 38.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 15:38:29
+2024년 1월 2일 16:1:37
 
 ### 문제 설명
 
--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int l, int r) {
+    public int[] solution(int l, int r) {        
         List<Integer> list = new ArrayList<>();
         for(int i = l; i<=r; i++) {
             String str = i + "";
             int count = 0;
             for(int j = 0; j < str.length(); j++) {
-                if(str.charAt(j) == 48 || str.charAt(j) == 53) {
+                if(str.charAt(j) == '0' || str.charAt(j) == '5') {
                     count ++;
                 }
             }
