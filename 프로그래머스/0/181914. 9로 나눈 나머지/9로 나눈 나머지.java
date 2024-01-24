2e32c28803fc432ba3e5f7721fbe515861510cc6
welcomeglory <153584777+welcomeglory@users.noreply.github.com>
2024-01-24 10:33:02 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(String number) {
-        int sum = 0;
-        for (char c : number.toCharArray()) {
-            sum += c - '0'; // char를 int로 변환하여 덧셈
-        }
-        return sum%9;
+      //  int answer = 0;
+      return Integer.parseInt(number)% 9;
+      //  return answer;
     }
 }
