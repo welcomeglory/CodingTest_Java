6497fc9c1ce20d9ee948fad8de83126a2218c8c5
welcomeglory <153584777+welcomeglory@users.noreply.github.com>
2024-01-31 11:09:34 +0900

    [level 0] Title: 배열 만들기 3, Time: 2.91 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=intervals[0][0]; i<=intervals[0][1]; i++)
+            list.add(arr[i]);
+        for(int i=intervals[1][0]; i<=intervals[1][1]; i++)
+            list.add(arr[i]);        
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
